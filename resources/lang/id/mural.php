bccbff0fd4aa4ff5e046c88c3efdfc3d04b8d79c
uyab <uyab.exe@gmail.com>
2015-08-13 14:59:02 +0700

    publish language file

---
+++ resources/lang/id/mural.php
@@ -0,0 +1,10 @@
+<?php
+
+return [
+    'must_login' => 'Silakan <a href=":link">login</a> untuk menuliskan komentar.',
+    'title_with_count' => ':count Komentar',
+    'write_a_comment'   => 'Tulis komentarmu disini...',
+    'submit'    => 'Submit',
+    'load_more' => 'Tampilkan lebih banyak',
+    'no_more_content' => 'Tidak ada lagi komentar',
+];
--- src/ServiceProvider.php
+++ src/ServiceProvider.php
@@ -21,6 +21,7 @@ class ServiceProvider extends BaseServiceProvider
         $this->publishes([
             __DIR__.'/../config/mural.php' => config_path('mural.php'),
             __DIR__.'/../database/migrations/2015_08_17_101000_create_comments_table.php' => database_path('migrations/2015_08_17_101000_create_comments_table.php'),
+            __DIR__.'/../resources/lang/id' => base_path('resources/lang/id'),
         ]);
 
         $this->mergeConfigFrom(
