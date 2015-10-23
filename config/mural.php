b1aee7b228b245ec850f3a3f59a36686eb926201
uyab <uyab.exe@gmail.com>
2015-10-23 15:00:53 +0700

    add note to config

--- config/mural.php
+++ config/mural.php
@@ -8,5 +8,6 @@ return [
     'per_page'            => 5,
 
     // whether user enable to vote comment or not
+    // if set true, you must install laravolt/votee package (https://github.com/laravolt/votee)
     'vote'                => false,
 ];
