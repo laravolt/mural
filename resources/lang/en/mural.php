2201869c0ed1aa3547108041463a3ba20de7d126
Bayu Hendra <bayu.hendra@javan.co.id>
2016-05-27 16:04:40 +0700

    translation

--- resources/lang/en/mural.php
+++ resources/lang/en/mural.php
@@ -1,10 +1,18 @@
 <?php
 
 return [
-    'must_login' => 'You must <a href=":link">login</a> to write a comment.',
+    'must_login'       => 'You must <a href=":link">login</a> to write a comment.',
     'title_with_count' => ':count Comment(s)',
-    'write_a_comment'   => 'Write a comment here...',
-    'submit'    => 'Submit',
-    'load_more' => 'More',
-    'no_more_content' => 'No more comment',
+    'write_a_comment'  => 'Write a comment here...',
+    'submit'           => 'Submit',
+    'load_more'        => 'More',
+    'no_more_content'  => 'No more comment',
+    'empty'            => 'This post has not received any comments.',
+    'remove'           => 'Remove',
+    'loading'          => 'Loading',
+    'sort'             => [
+        'latest' => 'Newest',
+        'liked'  => 'Most Liked',
+    ],
+
 ];
