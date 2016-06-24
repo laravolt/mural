e320cb6c9cfb573b25086fbb1378f63120e287dc
tama <am.adhatama@gmail.com>
2016-06-24 16:44:01 +0700

    add comment in test

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -6,13 +6,19 @@ use Laravolt\Mural\Comment;
 
 class TestCase extends \Orchestra\Testbench\TestCase
 {
-
+    /**
+     * setUp() akan dipanggil setiap method test case.
+     * Jadi setiap test case akan memiliki fresh data.
+     */
 	public function setUp()
     {
         parent::setUp();
         $this->setUpDatabase();
     }
 
+    /**
+     * Fungsinya mirip seperti config/app.php bagian registering Service Provider di Laravel apps
+     */
     protected function getPackageProviders($app)
     {
         return [
@@ -20,6 +26,9 @@ class TestCase extends \Orchestra\Testbench\TestCase
         ];
     }
 
+    /**
+     * Fungsinya mirip seperti config/app.php bagian registering Facade di Laravel apps
+     */
     protected function getPackageAliases($app)
     {
         return [
@@ -27,6 +36,13 @@ class TestCase extends \Orchestra\Testbench\TestCase
         ];
     }
 
+    /**
+     * Setup-setup config disini.
+     * Package Mural memiliki dependency pada config/mural.php, namun phpunit tidak otomatis mendeteksi config tersebut.
+     * Maka dari itu kita perlu ngeset manual setiap config yang diperlukan package kita untuk berjalan.
+     *
+     * Database menggunakan database.sqlite file yang akan digenerate di folder /tests.
+     */
     protected function getEnvironmentSetUp($app)
     {
         $app['config']->set('database.default', 'sqlite');
@@ -40,6 +56,19 @@ class TestCase extends \Orchestra\Testbench\TestCase
         $app['config']->set('mural.skin', 'semantic-ui');
     }
 
+    /**
+     * Setup database untuk dummy data yang dibutuhkan untuk testing.
+     * Method ini akan dijalankan setiap test case.
+     *
+     * File database.sqlite akan dinull kan sehingga test case akan memiliki fresh data setiap dijalankan.
+     *
+     * Penggunaan package Mural bergantung pada Model di Laravel apps seperti kewajiban implement interface di model atau
+     * penggunaan trait.
+     * Maka kita perlu membuat dummy model (DummyPost, DummyUser) agar dapat mensimulasikan situasi tersebut, beserta table-tablenya.
+     * Disini dilakukan schema creation untuk dummy-dummy model tersebut.
+     *
+     * Package Mural membutuhkan table db tersendiri (table Comment), maka kita perlu memigratekannya juga.
+     */
     protected function setUpDatabase()
     {
         file_put_contents(__DIR__.'/database.sqlite', null);
@@ -60,7 +89,6 @@ class TestCase extends \Orchestra\Testbench\TestCase
             '--database' => 'sqlite',
             '--realpath' => realpath(__DIR__.'/../database/migrations')
         ]);
-
 
         $post = DummyPost::create(['content' => 'My first post']);
         $user = DummyUser::create(['name' => 'Heru']);
